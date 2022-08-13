6e351cdc14eedec359845b77e53489fde441daf5
kriptonian <shymal.sawan@gmail.com>
2022-08-13 13:07:42 +0530

    style(pages): Add clean import from pages

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import Landing from "./Landing";
+import Home from "./Home";
+import Compiler from "./Compiler";
+import Codes from "./Codes";
+import Signin from "./Signin";
+import Profile from "./Profile";
+import ProfileSettings from "./ProfileSettings";
+
+export { Landing, Home, Compiler, Codes, Signin, Profile, ProfileSettings };
